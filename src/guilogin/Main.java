fde036eaf4212da88912acacc1e896e80acbe554
Youmeng <youmenghin15@gmail.com>
2021-08-01 16:04:49 -0400

    connect fxml file to main class

--- src/guilogin/Main.java
+++ src/guilogin/Main.java
@@ -1,6 +1,9 @@
 package guilogin;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,6 +12,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        
+
         stage.show();
     }
 }
