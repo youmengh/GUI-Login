3f8913353392d2e1178f08d917c442f6127fccfe
Youmeng <youmenghin15@gmail.com>
2021-08-01 15:13:23 -0400

    main class extends javafx application class

--- src/guilogin/Main.java
+++ src/guilogin/Main.java
@@ -1,5 +1,13 @@
 package guilogin;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
 }
